05138a0abbeb8e150a0be902a8867397f63b8487
davitlabadze <labadzedato18@gmail.com>
2023-03-01 17:24:10 +0400

    page links

--- about.view.php
+++ about.view.php
@@ -23,11 +23,10 @@
                         <div class="hidden md:block">
                             <div class="ml-10 flex items-baseline space-x-4">
                                 <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                                <a href="/" class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Home</a>
+                                <a href="/" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Home</a>
 
-                                <a href="/about.php" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">About</a>
+                                <a href="/about.php" class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium">About</a>
                                 <a href="/contact.php" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Contact</a>
-
 
                             </div>
                         </div>
@@ -131,12 +130,12 @@
 
         <header class="bg-white shadow">
             <div class="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
-                <h1 class="text-3xl font-bold tracking-tight text-gray-900">Dashboard</h1>
+                <h1 class="text-3xl font-bold tracking-tight text-gray-900">About</h1>
             </div>
         </header>
         <main>
             <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-                <p>Home</p>
+                <p>About</p>
             </div>
         </main>
     </div>
--- index.php
+++ index.php
@@ -1,23 +1,3 @@
 <?php
-$books = [
-    [
-        'name' => 'Do Androids Dream of Electroc Sheep',
-        'author' => 'Philip K. Dick',
-        'relaseYear' => "2015",
-        'purchaseUrl' => 'http://example.com'
-    ],
-    [
-        'name' => 'Project Mail Mary',
-        'author' => 'Andy Scott',
-        'relaseYear' => "2011",
-        'purchaseUrl' => 'http://example.com'
-    ]
-];
-
-
-$filteredBooks = array_filter($books, function ($book) {
-    return $book['relaseYear'] == 2015;
-});
-
 
 require "index.view.php";
