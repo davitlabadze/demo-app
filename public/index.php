b5651e9360ce04353c91714245ecf86bfef209ef
davitlabadze <labadzedato18@gmail.com>
2024-06-23 15:21:57 +0400

    feat: composer and autoloading

--- public/index.php
+++ public/index.php
@@ -5,14 +5,10 @@ use Core\ValidationException;
 
 const BASE_PATH = __DIR__ . '../../';
 
-require BASE_PATH . 'Core/functions.php';
-
-spl_autoload_register(function ($class) {
-    $class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
-    require base_path("{$class}.php");
-});
+require BASE_PATH . 'vendor/autoload.php';
 
 session_start();
+require BASE_PATH . 'Core/functions.php';
 require base_path('bootstrap.php');
 
 $router = new \Core\Router();
